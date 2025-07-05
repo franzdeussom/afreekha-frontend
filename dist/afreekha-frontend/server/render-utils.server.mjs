0a0e1a5f5a753e6f1f2c021f65c3c80639d49790
Franz Deussom <franzdeussom111@gmail.com>
2025-07-05 14:32:58 +0200

    delivery version: update faqs 05/07

--- dist/afreekha-frontend/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{c as o,d as r,e as n}from"./chunk-AA3LAAZH.mjs";import"./chunk-J4BDXTMW.mjs";import{uc as e}from"./chunk-CC2U4JK6.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
